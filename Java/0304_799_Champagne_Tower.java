315377e2d8b2208083d0296b3bed677fd3fda4b6
Daniel Lee <rjalths0303@gmail.com>
2022-03-04 12:34:15 -0500

    March 4 (Champagne Tower) Java

---
+++ Java/0304_799_Champagne_Tower.java
@@ -0,0 +1,27 @@
+/*
+March Leetcoding Challenge Day 4
+799. Champagne Tower
+[Solution]
+Language: Java
+Time Complexity: O(n^2)
+Space Complexity: O(n^2)
+where n is query_row
+*/
+class Solution {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        if (poured == 0) return 0.0;
+        double[][] glasses = new double[query_row+1][query_row+1];
+        glasses[0][0] = poured;
+        for (int r = 1; r <= query_row; r++) {
+            for (int c = 0; c <= r; c++){
+                if (c > 0 && glasses[r-1][c-1] > 1) {
+                    glasses[r][c] += (glasses[r-1][c-1] - 1) / 2.0;
+                }
+                if (c < r && glasses[r-1][c] > 1) {
+                    glasses[r][c] += (glasses[r-1][c] - 1) / 2.0;
+                }
+            }
+        }
+        return glasses[query_row][query_glass]>1?1:glasses[query_row][query_glass];
+    }
+}
