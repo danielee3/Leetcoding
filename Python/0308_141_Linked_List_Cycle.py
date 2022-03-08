93464f04d0fb4d12237741c61f4edd0646e7145b
Daniel Lee <rjalths0303@gmail.com>
2022-03-08 01:37:52 -0500

    March 8 (Linked List Cycle) Python

---
+++ Python/0308_141_Linked_List_Cycle.py
@@ -0,0 +1,31 @@
+"""
+March Leetcoding Challenge Day 8
+141. Linked List Cycle
+[Solution]
+Language: Python
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head is None:
+            return False
+        if head.next is None:
+            return False
+        pointer = head
+        while pointer is not None and pointer.next is not None:
+            pointer = pointer.next.next
+            head = head.next
+            if head is pointer:
+                return True
+        return False
