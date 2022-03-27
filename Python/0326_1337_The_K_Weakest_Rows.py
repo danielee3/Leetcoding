f84534a2b2617491382b1cf289fa6130d85a3ea8
Daniel Lee <rjalths0303@gmail.com>
2022-03-27 15:51:50 -0400

    March 26 (The K Weakest Row in a Matrix) Python

---
+++ Python/0326_1337_The_K_Weakest_Rows.py
@@ -0,0 +1,32 @@
+"""
+March Leetcoding Challenge Day 26
+1337. The K Weakest Rows in a Matrix
+[Solution]
+Language: Python
+Time Complexity: O(n^2)
+Space Complexity: O(n) <- The output itself is O(n)
+"""
+class Solution(object):
+    def kWeakestRows(self, mat, k):
+        """
+        :type mat: List[List[int]]
+        :type k: int
+        :rtype: List[int]
+        """
+        lst = []
+        for row in mat:
+            lst.append(sum(row))
+        ans = []
+        
+        while k > 0:
+            minVal = 101
+            minIndex = -1
+            for i in range(len(lst)):
+                if lst[i] < minVal:
+                    minVal = lst[i]
+                    minIndex = i
+            lst[minIndex] = 100
+            ans.append(minIndex)
+            k -= 1
+        
+        return ans
