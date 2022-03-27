3208a29b6803e193a8b7f027a84d85714b2c2007
Daniel Lee <rjalths0303@gmail.com>
2022-03-26 20:41:04 -0400

    March 25 (Binary Search) Python

---
+++ Python/0325_704_Binary_Search.py
@@ -0,0 +1,27 @@
+"""
+March Leetcoding Challenge Day 25
+704. Binary Search
+[Solution]
+Language: Python
+Time Complexity: O(log n)
+Space Complexity: O(1)
+"""
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        mn = 0
+        mx = len(nums) - 1
+        index = mn + ((mx-mn)/2)
+        while mn <= mx:
+            if target == nums[index]:
+                return index
+            elif target < nums[index]:
+                mx = index-1
+            else:
+                mn = index+1
+            index = mn + ((mx-mn)/2)
+        return -1
